f0c759dd81927ecaaa9f6d8e014e289edf174910
jammyroll <121103824+jammyroll@users.noreply.github.com>
2023-03-08 14:56:02 +0000

    almost done

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,28 +3,28 @@ db.dropDatabase()
 
 db.bookings.insertMany([
 {
-    Guest_name : 'Chris Donnely',
-    Guest_Email : 'chris1234@yahoo.com',
-    Checked_In : 'No'
+    name : 'Chris Donnely',
+    email : 'chris1234@yahoo.com',
+    status : 'No'
 },
 {
-    Guest_name : 'Daniel Piper',
-    Guest_Email : 'dannyboy@aol.com',
-    Checked_In : 'Yes'
+    name : 'Daniel Piper',
+    email : 'dannyboy@aol.com',
+    status : 'Yes'
 },
 {
-    Guest_name : 'Phil McKrackin',
-    Guest_Email : 'pkrackin@msn.com',
-    Checked_In : 'No'
+    name : 'Phil McKrackin',
+    email : 'pkrackin@msn.com',
+    status : 'No'
 },
 {
-    Guest_name : 'El',
-    Guest_Email : 'strangerthings@gmail.com',
-    Checked_In : ''
+    name : 'El',
+    email : 'strangerthings@gmail.com',
+    status : ''
 },
 {
-    Guest_name : 'Vittoria Moresco',
-    Guest_Email : 'vittoria.moresco@hotmail.com',
-    Checked_In : ''
+    name : 'Vittoria Moresco',
+    email : 'vittoria.moresco@hotmail.com',
+    status : ''
 }
 ])
